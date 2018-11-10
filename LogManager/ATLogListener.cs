8bf050d3519f49eff11a4fb0e716e91c3090eb29
ZhangHui_Company_TR <739536111@qq.com>
2018-11-10 17:52:54 +0800

    feat:增加Log功能

---
+++ LogManager/ATLogListener.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace LogManager
+{
+    class ATLogListener : TraceListener
+    {
+        private string m_logFileName;
+
+        public ATLogListener()
+        {
+            if (!Directory.Exists(LogConstant.LogFolder))
+            {
+                Directory.CreateDirectory(LogConstant.LogFolder);
+            }
+
+            this.m_logFileName = LogConstant.LogFolder + "/" + string.Format("Log-{0}", DateTime.Now.ToString("yyyyMMdd"));
+        }
+
+
+
+        public override void Write(string message)
+        {
+            message = Format(message, "");
+            File.AppendAllText(m_logFileName, message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            message = Format(message, "");
+            File.AppendAllText(m_logFileName, message);
+        }
+
+        public override void WriteLine(object o)
+        {
+            string message = Format(o, "");
+            File.AppendAllText(m_logFileName, message);
+        }
+
+        public override void WriteLine(string message, string category)
+        {
+            message = Format(message, category);
+            File.AppendAllText(m_logFileName, message);
+        }
+
+        public override void WriteLine(object o, string category)
+        {
+            string message = Format(o, category);
+            File.AppendAllText(m_logFileName, message);
+        }
+
+        private string Format(object obj, string category)
+        {
+            StringBuilder message = new StringBuilder();
+            message.AppendFormat("{0} ", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                message.AppendFormat("[{0}]", category);
+            }
+
+            if (obj is Exception)
+            {
+                var ex = obj as Exception;
+                message.Append(ex.Message + "\r\n");
+                message.Append(ex.StackTrace + "\r\n");
+            }
+            else
+            {
+                message.Append(obj.ToString() + "\r\n");
+            }
+
+            return message.ToString();
+        }
+    }
+}
---
+++ LogManager/LogManager.cs
@@ -0,0 +1,58 @@
+﻿#define TRACE
+
+using System.Diagnostics;
+using System.IO;
+
+namespace LogManager
+{
+    public class ATLog
+    {
+        static ATLog()
+        {
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new ATLogListener());
+        }
+
+        private static bool m_openLog = true;
+        /// <summary>
+        /// Log开关
+        /// </summary>
+        public static bool OpenLog
+        {
+            get { return m_openLog; }
+            set { m_openLog = value; }
+        }
+
+        public static void Debug(object msg)
+        {
+            Trace.WriteLineIf(m_openLog, msg, "Debug");
+        }
+
+        public static void Error(object msg)
+        {
+            Trace.WriteLineIf(m_openLog, msg, "Error");
+        }
+
+        public static void Info(object msg)
+        {
+            Trace.WriteLineIf(m_openLog, msg, "Info");
+        }
+
+        public static void Warn(object msg)
+        {
+            Trace.WriteLineIf(m_openLog, msg, "Warn");
+        }
+
+        public static void ClearLog()
+        {
+            if (Directory.Exists(LogConstant.LogFolder))
+            {
+                string[] logsPath = Directory.GetFiles(LogConstant.LogFolder);
+                for (int i = 0; i < logsPath.Length; i++)
+                {
+                    File.Delete(logsPath[i]);
+                }
+            }
+        }
+    }
+}
